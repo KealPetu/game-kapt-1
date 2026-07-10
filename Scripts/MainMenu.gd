48a4e9c8663d488c6431f40c8c2720e797fb6dfa
KealPetu <kpenafiel99@gmail.com>
2026-07-10 07:20:29 -0500

    MainMenu scene structure changed

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,22 +1,22 @@
-extends Node
+extends Control
 
-@onready var credits_layer: CanvasLayer = $CreditsLayer
 
 # Main Options
-@onready var main_options_layer: CanvasLayer = $MainOptionsLayer
-@onready var play_button: Button = $MainOptionsLayer/MainOptions/OptionsContainer/PlayButton
-@onready var credits_button: Button = $MainOptionsLayer/MainOptions/OptionsContainer/CreditsButton
-@onready var exit_button: Button = $MainOptionsLayer/MainOptions/OptionsContainer/ExitButton
+@onready var main_options: Control = $OptionsContainer
+@onready var play_button: Button = $OptionsContainer/PlayButton
+@onready var credits_button: Button = $OptionsContainer/CreditsButton
+@onready var exit_button: Button = $OptionsContainer/ExitButton
 
 # Credits Options
-@onready var back_button: Button = $CreditsLayer/CreditsPanel/Panel/BackButton
+@onready var credits_panel: Panel = $CreditsPanel
+@onready var back_button: Button = $CreditsPanel/BackButton
 
 func _ready() -> void:
 	play_button.pressed.connect(_on_play_pressed)
 	credits_button.pressed.connect(_on_credits_pressed)
 	exit_button.pressed.connect(_on_exit_pressed)
 	back_button.pressed.connect(_on_back_pressed)
-	credits_layer.hide()
+	credits_panel.hide()
 
 
 func _on_play_pressed():
@@ -25,13 +25,13 @@ func _on_play_pressed():
 
 
 func _on_credits_pressed():
-	main_options_layer.hide()
-	credits_layer.show()
+	main_options.hide()
+	credits_panel.show()
 
 
 func _on_back_pressed():
-	main_options_layer.show()
-	credits_layer.hide()
+	main_options.show()
+	credits_panel.hide()
 
 
 func _on_exit_pressed():
